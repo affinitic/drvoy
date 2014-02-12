102f46ddf2c9e67e8e26490e5ab7b5e6f4dc05a2
Alain Meurant <alain.meurant@affinitic.be>
2014-02-12 16:57:58 +0100

    Au second instant, Il lacha le gros paquet

---
+++ src/drvoy/skin/browser/interfaces.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IdrvoyTheme(IDefaultPloneLayer):
+    """
+    Theme for drvoy
+    """
