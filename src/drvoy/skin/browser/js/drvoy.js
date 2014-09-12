9b3a35bb96bf53f8fba0265a737aba90c06259c4
Alain Meurant <alain.meurant@affinitic.be>
2014-09-12 16:34:53 +0200

    JS-Supersizer

--- src/drvoy/skin/browser/js/drvoy.js
+++ src/drvoy/skin/browser/js/drvoy.js
@@ -5,10 +5,10 @@ jQuery(function($){
 		transition : 1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
 		random : 1,
 		transition_speed : 2000,		// Speed of transition
-
-		// Components
+															   
+		// Components							
 		slide_links : 'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		slides:[
+		slides:[ 
 		        {image : 'drvoy_bg_01.jpg'},
 		        {image : 'drvoy_bg_02.jpg'},
 		        {image : 'drvoy_bg_03.jpg'},
