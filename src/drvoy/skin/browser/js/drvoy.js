bb2f34fc89e85de501cec3d5ee66be3caadc8370
Alain Meurant <alain.meurant@affinitic.be>
2015-11-01 16:35:29 +0100

    DR VOY - V2.0
    Changement de la skin

--- src/drvoy/skin/browser/js/drvoy.js
+++ src/drvoy/skin/browser/js/drvoy.js
@@ -1,14 +1,14 @@
-jQuery(function($){
-    $.supersized({
+//jQuery(function($){
+//    $.supersized({
 	    // Functionality
-		slide_interval : 4000,		// Length between transitions
-		transition : 1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		random : 1,
+		//slide_interval : 4000,		// Length between transitions
+		//transition : 1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+		//random : 1,
 		transition_speed : 2000,		// Speed of transition
 
 		// Components
-		slide_links : 'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-		slides:[
+		//slide_links : 'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
+		/*slides:[
 		        {image : 'drvoy_bg_01.jpg'},
 		        {image : 'drvoy_bg_02.jpg'},
 		        {image : 'drvoy_bg_03.jpg'},
@@ -20,6 +20,6 @@ jQuery(function($){
 		        {image : 'drvoy_bg_09.jpg'},
 		        {image : 'drvoy_bg_10.jpg'},
 		        {image : 'drvoy_bg_11.jpg'}
-		        ]
-		});
-	});
+		        ]*/
+//		});
+//	});
